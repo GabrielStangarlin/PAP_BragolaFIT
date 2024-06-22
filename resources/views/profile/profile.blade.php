06b4edb926e4465e2f5cc2cfa23d2bf64823f4f8
GabrielStangarlin <163768616+GabrielStangarlin@users.noreply.github.com>
2024-06-22 21:36:54 +0100

    n liga

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -79,11 +79,11 @@
         </div>
     </div>
 
-{{-- <div id="successModal" class="modal-content p-3 mt-5">
+<div id="successModal" class="modal-content p-3 mt-5">
                         <div class="modal-body">
                             <p id="successMessage"></p>
                         </div>
-                    </div> --}}
+                    </div>
 
 
     <div class="container main-container">
@@ -96,11 +96,11 @@
         </div>
         <div class="right-column">
             <div id="perfil" class="content-section">
-                {{-- @if (session('success'))
+                @if (session('success'))
                 <script>
                     const successMessage = "{{ session('success') }}";
                 </script>
-            @endif --}}
+            @endif
                 <div class="card p-4">
                     <form action="{{ route('user.updateProfile') }}" method="POST">
                         @csrf
