642146c429d7ca85fb0a3d952bb1c08af34d2cbe
GabrielStangarlin <gabrielstang10@gmail.com>
2024-07-12 00:39:31 +0100

    joao me deve 1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel(['resources/css/app.css', 'resources/js/app.js', 'resources/css/style.css']),
+        laravel(['resources/css/app.css', 'resources/js/app.js', 'resources/css/styles.css']),
     ],
 });
